e5518274e202d1fdb7e541bb40b325a04fa5c237
QuackHack-McBlindy <isthisrandomenough@protonmail.com>
2025-02-25 08:55:38 +0100

    Updated files

--- modules/services/syncthing.nix
+++ modules/services/syncthing.nix
@@ -22,7 +22,7 @@
                 "desktop" = { id = "6RIE3DZ-XWAP6NX-OF2JF4Z-N35U3RZ-IL5FX7H-AFWROYH-5HMM35U-WQHY4Q3"; };
             #    "laptop" = { id = "DEVICE-ID-2"; autoAcceptFolders = true; };
                 "homie" = { id = "R4DUXJ4-IBMIMIU-Y5ROSQV-7HHWSCH-QCUV7XX-ZSE6ZEG-HGPYK3D-YC5E6A2"; };
-                "nasty" = { id = "JKK3F7P-P23AKMA-CSG7CDI-WZQBVW3-PLXKSWA-CLHG272-M2AQ2ZW-P3HTRA6"; };
+                "nasty" = { id = "6DNQMMQ-MGUYQRJ-PIDGQLA-7ZLSJMO-L4E5LC3-RKAZEBE-LAAIA2A-PH5ZLQR"; };
             };
 
             folders."dotfiles" = {
