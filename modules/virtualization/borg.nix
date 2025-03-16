b4fbea9f7019638b1af1d254d4b8af2f56643f6a
QuackHack-McBlindy <isthisrandomenough@protonmail.com>
2025-03-16 06:17:41 +0100

    Updated files

--- modules/virtualization/borg.nix
+++ modules/virtualization/borg.nix
@@ -4,8 +4,77 @@
     pkgs,
     ...
 } : let
-    pubkey = import ./../../hosts/pubkeys.nix;
-    #borg-image = import /docker/borg/borg.tar;
+
+    borgImage = pkgs.dockerTools.buildImage {
+        name = "borg";
+        tag = "latest";
+
+        copyToRoot = pkgs.buildEnv {
+            name = "image-root";
+            paths = with pkgs; [
+                bash
+                shadow
+                openssh
+                sudo
+                toybox
+                busybox
+                debianutils
+            ];
+            pathsToLink = [ "/bin" "/etc" "/usr" "/var" ];
+        };
+
+        runAsRoot = ''
+            #!${pkgs.runtimeShell}
+            ${pkgs.dockerTools.shadowSetup}
+            groupadd sudo
+            useradd -m -s ${pkgs.bash}/bin/bash borg
+            adduser borg sudo
+            mkdir -p /run/sshd
+            mkdir -p /home/borg/.ssh
+            chmod 700 /home/borg/.ssh
+            chown borg:borg /home/borg/.ssh
+            echo "PubkeyAuthentication yes" >> /etc/ssh/sshd_config
+            mkdir -p /etc/ssh/keys
+            ${pkgs.openssh}/bin/ssh-keygen -t rsa -b 4096 -f /etc/ssh/keys/ssh_host_rsa_key -N ""
+            ${pkgs.openssh}/bin/ssh-keygen -t ecdsa -b 521 -f /etc/ssh/keys/ssh_host_ecdsa_key -N ""
+            ${pkgs.openssh}/bin/ssh-keygen -t ed25519 -f /etc/ssh/keys/ssh_host_ed25519_key -N ""
+            cp ${entrypointScript} /bin/entrypoint.sh
+            chmod +x /bin/entrypoint.sh
+        '';
+
+        config = {
+            Cmd = [ "/bin/entrypoint.sh" ];
+            ExposedPorts = {
+                "2222/tcp" = {};
+            };
+            WorkingDir = "/home/borg";
+            Volumes = {
+               "/etc/ssh/keys" = {};
+               "/home/borg" = {};
+            };
+        };
+    };
+
+    entrypointScript = pkgs.writeScript "entrypoint.sh" ''
+        #!/bin/bash
+        if [ -n "$AUTHORIZED_KEYS" ]; then
+            echo "$AUTHORIZED_KEYS" > /home/borg/.ssh/authorized_keys
+            chmod 600 /home/borg/.ssh/authorized_keys
+            chown borg:borg /home/borg/.ssh/authorized_keys
+        fi
+
+        if [ "$PROTECTION" = "on" ]; then
+            echo "PROTECTION mode enabled: Only public key authentication allowed."
+            sed -i "s/#PasswordAuthentication yes/PasswordAuthentication no/" /etc/ssh/sshd_config
+        elif [ "$PROTECTION" = "off" ]; then
+            echo "PROTECTION mode disabled: Allowing password authentication."
+            sed -i "s/#PasswordAuthentication no/PasswordAuthentication yes/" /etc/ssh/sshd_config
+            echo "borg:borg" | chpasswd
+        fi
+        exec /usr/sbin/sshd -D
+    '';
+
+
     Dockerfile = pkgs.writeText "Dockerfile" ''
         FROM ubuntu:latest
         RUN apt-get update && apt-get install -y \
@@ -78,14 +147,14 @@
 in {
 # sudo chown -R dockeruser:dockeruser /docker/borg
 # sudo chmod -R 755 /docker/borg
-
+    pubkey = import ./../../hosts/pubkeys.nix;
 
     virtualisation.oci-containers = {
         backend = "docker";
         containers = {
             borgbackup = {
-               # image = "borg-borgbackup";
-               # imageFile = borg-image;
+                imageFile = borgImage;
+                image = "borg:latest";
                 hostname = "borg";
                 user = "977:968"; 
                 autoStart = false;
@@ -116,18 +185,14 @@ in {
             ${pkgs.coreutils}/bin/cp ${run} /docker/borg/run.sh
             ${pkgs.coreutils}/bin/cp ${entrypoint} /docker/borg/entrypoint.sh
             
-          #  ${pkgs.docker}/bin/docker images -q borg-borgbackup:latest || \
-         #   ${pkgs.docker}/bin/docker build -t borg-borgbackup /docker/borg     
-            
             if ! ${pkgs.docker}/bin/docker network ls | grep -q "borgnet"; then
                 ${pkgs.docker}/bin/docker network create --subnet=10.10.10.0/24 borgnet
-            fi
+            fi 
 
         '';
     
         serviceConfig = {
-            ExecStart = "${pkgs.bash}/bin/bash -c 'bash run.sh'";
-         #   ${pkgs.docker}/bin/bash -c 'echo Sucessfully setup borg backup server; '";
+            ExecStart = "${pkgs.docker}/bin/bash -c 'echo Sucessfully setup borg backup server; '";
             Restart = "on-failure";
             RestartSec = "2s";
             RuntimeDirectory = [ "/docker/borg" ];
