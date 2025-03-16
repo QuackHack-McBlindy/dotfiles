bc4224903d5f18449372f9c8e109b816ba4edb5f
QuackHack-McBlindy <isthisrandomenough@protonmail.com>
2025-03-16 08:46:46 +0100

    Updated files

--- modules/virtualization/borgServer.nix
+++ modules/virtualization/borgServer.nix
@@ -1,184 +1,134 @@
 { 
-    config,
-    lib,
-    pkgs,
-    ...
-} : let
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
-    pubkey = import ./../../hosts/pubkeys.nix;
+let
+  # Import public keys
+  pubkey = import ./../../hosts/pubkeys.nix;
 
+  # Define the entrypoint script
+  entrypointScript = pkgs.writeScript "entrypoint.sh" ''
+    #!/bin/bash
+    mkdir -p /home/borg/.ssh
+    if [ -n "$AUTHORIZED_KEYS" ]; then
+      echo "$AUTHORIZED_KEYS" > /home/borg/.ssh/authorized_keys
+      chmod 600 /home/borg/.ssh/authorized_keys
+      chown borg:borg /home/borg/.ssh/authorized_keys
+    fi
 
-    entrypointScript = pkgs.writeScript "entrypoint.sh" ''
-        #!/bin/bash
-        mkdir -p /home/borg/.ssh
-        if [ -n "$AUTHORIZED_KEYS" ]; then
-            echo "$AUTHORIZED_KEYS" > /home/borg/.ssh/authorized_keys
-            chmod 600 /home/borg/.ssh/authorized_keys
-            chown borg:borg /home/borg/.ssh/authorized_keys
-        fi
+    if [ "$PROTECTION" = "on" ]; then
+      echo "PROTECTION mode enabled: Only public key authentication allowed."
+      sed -i "s/#PasswordAuthentication yes/PasswordAuthentication no/" /etc/ssh/sshd_config
+    elif [ "$PROTECTION" = "off" ]; then
+      echo "PROTECTION mode disabled: Allowing password authentication."
+      sed -i "s/#PasswordAuthentication no/PasswordAuthentication yes/" /etc/ssh/sshd_config
+      echo "borg:borg" | chpasswd
+    fi
+    exec /usr/sbin/sshd -D
+  '';
 
-        if [ "$PROTECTION" = "on" ]; then
-            echo "PROTECTION mode enabled: Only public key authentication allowed."
-            sed -i "s/#PasswordAuthentication yes/PasswordAuthentication no/" /etc/ssh/sshd_config
-        elif [ "$PROTECTION" = "off" ]; then
-            echo "PROTECTION mode disabled: Allowing password authentication."
-            sed -i "s/#PasswordAuthentication no/PasswordAuthentication yes/" /etc/ssh/sshd_config
-            echo "borg:borg" | chpasswd
-        fi
-        exec /usr/sbin/sshd -D
+  # Define the Docker image
+  borgImage = pkgs.dockerTools.buildImage {
+    name = "borg";
+    tag = "latest";
+
+    contents = [
+      pkgs.bash
+      pkgs.shadow
+      pkgs.openssh
+      pkgs.sudo
+      pkgs.toybox
+      pkgs.busybox
+      pkgs.debianutils
+    ];
+
+    runAsRoot = ''
+      #!${pkgs.runtimeShell}
+      ${pkgs.dockerTools.shadowSetup}
+      groupadd sudo
+      useradd -m -s ${pkgs.bash}/bin/bash borg
+      adduser borg sudo
+      mkdir -p /run/sshd
+      mkdir -p /home/borg/.ssh
+      chmod 700 /home/borg/.ssh
+      chown borg:borg /home/borg/.ssh
+      echo "PubkeyAuthentication yes" >> /etc/ssh/sshd_config
+      chmod 644 /etc/ssh/sshd_config # Ensure the file is writable
+      mkdir -p /etc/ssh/keys
+      ${pkgs.openssh}/bin/ssh-keygen -t rsa -b 4096 -f /etc/ssh/keys/ssh_host_rsa_key -N ""
+      ${pkgs.openssh}/bin/ssh-keygen -t ecdsa -b 521 -f /etc/ssh/keys/ssh_host_ecdsa_key -N ""
+      ${pkgs.openssh}/bin/ssh-keygen -t ed25519 -f /etc/ssh/keys/ssh_host_ed25519_key -N ""
+      cp ${entrypointScript} /bin/entrypoint.sh
+      chmod +x /bin/entrypoint.sh
     '';
 
-
-    borgImage = pkgs.dockerTools.buildImage {
-        name = "borg";
-        tag = "latest";
-        contents = [
-            pkgs.bash
-            pkgs.shadow
-            pkgs.openssh
-            pkgs.sudo
-            pkgs.toybox
-            pkgs.busybox
-            pkgs.debianutils
-        ];
-
-        runAsRoot = ''
-            #!${pkgs.runtimeShell}
-            ${pkgs.dockerTools.shadowSetup}
-            groupadd sudo
-            useradd -m -s ${pkgs.bash}/bin/bash borg
-            adduser borg sudo
-            mkdir -p /run/sshd
-            mkdir -p /home/borg/.ssh
-            chmod 700 /home/borg/.ssh
-            chown borg:borg /home/borg/.ssh
-            echo "PubkeyAuthentication yes" >> /etc/ssh/sshd_config
-            chmod 644 /etc/ssh/sshd_config # Ensure the file is writable
-            mkdir -p /etc/ssh/keys
-            ${pkgs.openssh}/bin/ssh-keygen -t rsa -b 4096 -f /etc/ssh/keys/ssh_host_rsa_key -N ""
-            ${pkgs.openssh}/bin/ssh-keygen -t ecdsa -b 521 -f /etc/ssh/keys/ssh_host_ecdsa_key -N ""
-            ${pkgs.openssh}/bin/ssh-keygen -t ed25519 -f /etc/ssh/keys/ssh_host_ed25519_key -N ""
-            cp ${entrypointScript} /bin/entrypoint.sh
-            chmod +x /bin/entrypoint.sh
-        '';
-
-        config = {
-            Cmd = [ "/bin/entrypoint.sh" ];
-            ExposedPorts = {
-                "2222/tcp" = {};
-            };
-            WorkingDir = "/home/borg"; # Absolute path
-            Volumes = {
-                "/etc/ssh/keys" = {};
-                "/home/borg" = {};
-            };
-        };
+    config = {
+      Cmd = [ "/bin/entrypoint.sh" ];
+      ExposedPorts = {
+        "2222/tcp" = {};
+      };
+      WorkingDir = "/home/borg"; # Absolute path
+      Volumes = {
+        "/etc/ssh/keys" = {};
+        "/home/borg" = {};
+      };
     };
+  };
 
-
-
-
-    Dockerfile = pkgs.writeText "Dockerfile" ''
-        FROM ubuntu:latest
-        RUN apt-get update && apt-get install -y \
-            openssh-server \
-            sudo \
-            toybox \
-            busybox \
-            debianutils \
-            && rm -rf /var/lib/apt/lists/*
-        RUN useradd -m -s /bin/bash borg && \
-            adduser borg sudo
-        RUN mkdir -p /run/sshd
-        RUN mkdir -p /home/borg/.ssh && chmod 700 /home/borg/.ssh && chown borg:borg /home/borg/.ssh
-        RUN echo "PubkeyAuthentication yes" >> /etc/ssh/sshd_config
-        EXPOSE 2222
-        RUN sed -i "s/#Port 22/Port 2222/" /etc/ssh/sshd_config && \
-        sed -i "s/#PasswordAuthentication yes/PasswordAuthentication no/" /etc/ssh/sshd_config
-        RUN mkdir -p /etc/ssh/keys && \
-            if [ ! -f /etc/ssh/keys/ssh_host_rsa_key ]; then \
-                ssh-keygen -t rsa -b 4096 -f /etc/ssh/keys/ssh_host_rsa_key -N ""; \
-            fi && \
-            if [ ! -f /etc/ssh/keys/ssh_host_ecdsa_key ]; then \
-                ssh-keygen -t ecdsa -b 521 -f /etc/ssh/keys/ssh_host_ecdsa_key -N ""; \
-            fi && \
-            if [ ! -f /etc/ssh/keys/ssh_host_ed25519_key ]; then \
-                ssh-keygen -t ed25519 -f /etc/ssh/keys/ssh_host_ed25519_key -N ""; \
-            fi
-        RUN echo "Host Public Key (RSA):" && cat /etc/ssh/keys/ssh_host_rsa_key.pub && \
-            echo "Host Public Key (ECDSA):" && cat /etc/ssh/keys/ssh_host_ecdsa_key.pub && \
-            echo "Host Public Key (ED25519):" && cat /etc/ssh/keys/ssh_host_ed25519_key.pub
-    '';
-
-    run = pkgs.writeText "run.sh" ''
-        docker run -d \
-            --name borgbackup \
-            --hostname borg \
-            --user 977:968 \
-            --restart unless-stopped \
-            -p 2225:2222 \
-            -e AUTHORIZED_KEYS="${pubkey.desktop} ${pubkey.homie} ${pubkey.nasty}" \
-            -e PROTECTION="on" \
-            -v /docker/borg:/etc/ssh/keys \
-            -v /docker/borg/entrypoint.sh:/bin/entrypoint.sh \
-            -v /backup/borg:/home/borg \
-            --network=borgnet \
-            --ip=10.10.10.2 \
-            borg-borgbackup \
-            /bin/entrypoint.sh
-    '';
-    
 in {
-# sudo chown -R dockeruser:dockeruser /docker/borg
-# sudo chmod -R 755 /docker/borg
-
-    virtualisation.oci-containers = {
-        backend = "docker";
-        containers = {
-            borgbackup = {
-                imageFile = borgImage;
-                image = "borg:latest";
-                hostname = "borg";
-                user = "977:968"; 
-                autoStart = true;
-                ports = [ "2223:2222" ];
-                environment = {                
-                    AUTHORIZED_KEYS = "${pubkey.desktop} ${pubkey.homie} ${pubkey.nasty}";
-                    PROTECTION="on";
-                };
-                volumes = [
-                    "/docker/borg:/etc/ssh/keys"
-                    "/docker/borg/entrypoint.sh:/bin/entrypoint.sh"
-                    "/backup/borg:/home/borg"
-                ];
-                extraOptions = [
-                   "--network=borgnet"
-                   "--ip=10.10.10.2"    
-                ];
-                entrypoint = "/bin/entrypoint.sh";
-            };
-        };    
-    };
-    
-    systemd.services.borg-setup = {
-        wantedBy = [ "multi-user.target" ];
-        preStart = ''
-            ${pkgs.coreutils}/bin/mkdir -p /docker/borg
-            ${pkgs.coreutils}/bin/cp ${Dockerfile} /docker/borg/Dockerfile
-            ${pkgs.coreutils}/bin/cp ${run} /docker/borg/run.sh
-            ${pkgs.coreutils}/bin/cp ${entrypoint} /docker/borg/entrypoint.sh
-            
-            if ! ${pkgs.docker}/bin/docker network ls | grep -q "borgnet"; then
-                ${pkgs.docker}/bin/docker network create --subnet=10.10.10.0/24 borgnet
-            fi 
-        '';
-    
-        serviceConfig = {
-            ExecStart = "${pkgs.bash}/bin/bash -c 'echo Ready to receieve backups! at borg@10.10.10.2; '";
-            Restart = "on-failure";
-            RestartSec = "2s";
-            #RuntimeDirectory = [ "/docker/borg" ];
-            User = "dockeruser";
+  # Configure the Docker container
+  virtualisation.oci-containers = {
+    backend = "docker";
+    containers = {
+      borgbackup = {
+        imageFile = borgImage;
+        image = "borg:latest";
+        hostname = "borg";
+        user = "977:968"; 
+        autoStart = true;
+        ports = [ "2223:2222" ];
+        environment = {                
+          AUTHORIZED_KEYS = "${pubkey.desktop} ${pubkey.homie} ${pubkey.nasty}";
+          PROTECTION = "on";
         };
-    };}
+        volumes = [
+          "/docker/borg:/etc/ssh/keys"
+          "/docker/borg/entrypoint.sh:/bin/entrypoint.sh"
+          "/backup/borg:/home/borg"
+        ];
+        extraOptions = [
+          "--network=borgnet"
+          "--ip=10.10.10.2"    
+        ];
+        entrypoint = "/bin/entrypoint.sh";
+      };
+    };    
+  };
 
+  # Systemd service to set up directories and Docker network
+  systemd.services.borg-setup = {
+    wantedBy = [ "multi-user.target" ];
+    preStart = ''
+      ${pkgs.coreutils}/bin/mkdir -p /docker/borg
+      ${pkgs.coreutils}/bin/chown -R dockeruser:dockeruser /docker/borg
+      ${pkgs.coreutils}/bin/chmod -R 755 /docker/borg
+      ${pkgs.coreutils}/bin/cp ${entrypointScript} /docker/borg/entrypoint.sh
+      ${pkgs.coreutils}/bin/chmod +x /docker/borg/entrypoint.sh
+
+      if ! ${pkgs.docker}/bin/docker network ls | grep -q "borgnet"; then
+        ${pkgs.docker}/bin/docker network create --subnet=10.10.10.0/24 borgnet
+      fi
+    '';
+
+    serviceConfig = {
+      ExecStart = "${pkgs.coreutils}/bin/true"; # No-op, as setup is done in preStart
+      Restart = "on-failure";
+      RestartSec = "2s";
+      RuntimeDirectory = [ "/docker/borg" ];
+      User = "dockeruser";
+    };
+  };
+}
