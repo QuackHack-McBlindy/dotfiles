1e633e863041ea01d2ca13f6f87185f97261814f
QuackHack-McBlindy <isthisrandomenough@protonmail.com>
2025-03-22 15:12:00 +0100

    Updated files

--- modules/nixos/users.nix
+++ modules/nixos/users.nix
@@ -40,15 +40,22 @@ in
          # ];                  
       };
    
-      users.builder = if config.networking.hostName == "desktop" then {
+   #   users.builder = if config.networking.hostName == "desktop" then {
+   #       isNormalUser = true;
+   #       home = "/home/builder";
+   #       shell = pkgs.bash;
+   #     #  openssh.authorizedKeys.keys = [ pubkey.desktop pubkey.laptop pubkey.homie pubkey.nasty ];
+   #       extraGroups = [ "wheel" "builders" ]; 
+   #   } else
+   #       {}; 
+ 
+      users.builder = {
           isNormalUser = true;
           home = "/home/builder";
           shell = pkgs.bash;
-        #  openssh.authorizedKeys.keys = [ pubkey.desktop pubkey.laptop pubkey.homie pubkey.nasty ];
           extraGroups = [ "wheel" "builders" ]; 
-      } else
-          {}; 
-    
+      };
+ 
       users.caddyProxy = {
           group = "caddyProxy";
           home = "/var/lib/caddyProxy";
