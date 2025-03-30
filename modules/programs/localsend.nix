62b22f4bfc6c5ef86a6bee7528bc17a22ebe1869
QuackHack-McBlindy <isthisrandomenough@protonmail.com>
2025-03-30 16:56:55 +0200

    Updated files

--- modules/programs/localsend.nix
+++ modules/programs/localsend.nix
@@ -3,9 +3,27 @@
     lib, 
     pkgs, 
     ... 
-} : { 
+} : in
+    localsend = {
+        ip = "127.0.0.1";  
+        port = 53317;      
+    };
+let { 
+    networking.firewall.allowedTCPPorts = [ localsend.port ];
 
     programs.localsend = {
         enable = true;
         openFirewall = true;    
+    };
+        
+    services.nginx = {
+        enable = true;
+        recommendedProxySettings = true;
+        virtualHosts = {
+            "send" = {
+                locations."/".proxyPass = "http://${localsend.ip}:${toString localsend.port}";
+            };
+        };
+    };
+
     };}
