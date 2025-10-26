12bd1d120f779bc7da12b4ee5fc7b271d38ca13e
QuackHack-McBlindy <isthisrandomenough@protonmail.com>
2025-10-26 02:21:00 +0200

    Autocommit: Generation 303
    Changed files:\n README.md                                | 70 ++++++++++++++++----------------
     bin/media/vlc.nix                        | 22 ++--------
     modules/services/file-server/browse.html |  2 +-
     secrets/caddyfile.yaml                   | 16 ++++----
     4 files changed, 48 insertions(+), 62 deletions(-)

--- bin/media/vlc.nix
+++ bin/media/vlc.nix
@@ -39,24 +39,10 @@ in {
     autoStart = false;
     logLevel = "INFO";
     parameters = [
-      { name = "typ"; description = "Specify the type of command or the media type to search for. Supported commands: on, off, up, down, call, favorites, add. Media Types: tv, movie, livetv, podcast, news, music, song, musicvideo, jukebox (random music), othervideo, youtube, nav_up, nav_down, nav_left, nav_right, nav_select, nav_menu, nav_back"; default = "tv"; optional = true; }
-      { name = "search"; type = "string"; description = "Media to search"; optional = true; }
-      { name = "device"; description = "Device IP to play on"; default = "192.168.1.223"; }      
-      { name = "shuffle"; type = "bool"; description = "Shuffle Toggle, true or false"; default = true; }   
-      { name = "tvshowsDir"; type = "path"; description = "TV shows directory"; default = "/Pool/TV"; }
-      { name = "moviesDir"; type = "path"; description = "Movies directory"; default = "/Pool/Movies"; }
-      { name = "musicDir"; type = "path"; description = "Music directory"; default = "/Pool/Music"; }
-      { name = "musicvideoDir"; type = "path"; description = "Music videos directory"; default = "/Pool/Music_Videos"; }      
-      { name = "videosDir"; type = "path"; description = "Other videos directory"; default = "/Pool/Other_Videos"; }
-      { name = "podcastDir"; type = "path"; description = "Podcasts directory"; default = "/Pool/Podcasts"; }
-      { name = "audiobookDir"; type = "path"; description = "Audiobooks directory"; default = "/Pool/Audiobooks"; }
-      { name = "youtubeAPIkeyFile"; type = "path"; description = "File containing YouTube API key"; default = config.sops.secrets.youtube_api_key.path; }
-      { name = "webserver"; type = "path"; description = "File containing webserver URL that stores media"; default = config.sops.secrets.webserver.path; }     
-      { name = "defaultPlaylist"; description = "Default playlist path"; default = "/Pool/playlist.m3u"; }
-      { name = "favoritesPlaylist"; description = "File path for Favouyrites tagged entries"; default = "/Pool/favorites.m3u"; }      
-      { name = "max_items"; type = "int"; description = "Set a maximum number of items in playlist"; default = 200; }       
-      { name = "mqttUser"; type = "string"; description = "User which Mosquitto runs on"; default = "mqtt"; optional = false; }
-      { name = "mqttPWFile"; type = "path"; description = "Password file for Mosquitto user"; optional = false; default = config.sops.secrets.mosquitto.path; }
+      { name = "add"; type = "path"; description = "Append file path to playlist"; optional = true; }
+      { name = "add_folder"; type = "path"; description = "Append directory path to playlist"; }      
+      { name = "remove"; type = "path"; description = "Remove file path from playlist"; }      
+
     ];
     code = ''
       ${cmdHelpers}
